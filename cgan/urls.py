e1d6228bcc4ae0f7dee9c40d8c38329ce7836a86
kozistr <kozistr@gmail.com>
2018-08-15 04:15:23 +0900

    fix: view obj

--- cgan/urls.py
+++ cgan/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^about/$', view=views.about, name='about'),
-    url(r'^demo/$', view=views.gen_cgan_image, name='demo'),
+    url(r'^demo/$', view=views.gan_load, name='demo'),
 ]
