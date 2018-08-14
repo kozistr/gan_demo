adf57af8fb953a065c1eef14fcf046b56e438f7d
kozistr <kozistr@gmail.com>
2018-08-15 03:54:37 +0900

    fix: return type

--- cgan/views.py
+++ cgan/views.py
@@ -8,8 +8,9 @@ CGAN_PAGE = 'cgan/cgan.html'
 
 def gan_load(req):
     wanna_label = 1  # will be replaced with custom input
-    if gen_cgan_image(wanna_label):
-        return render(req, CGAN_PAGE, {})
+
+    gen_cgan_image(wanna_label)
+    return render(req, CGAN_PAGE, {})
 
 
 def about(req):
