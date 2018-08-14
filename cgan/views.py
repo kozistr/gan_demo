3d4f69d4983f9aa5cba7694b318dd57ffa0d1b33
kozistr <kozistr@gmail.com>
2018-08-15 03:33:48 +0900

    update: view inference

--- cgan/views.py
+++ cgan/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+
+CGAN_PAGE = 'cgan/cgan.html'
+
+
+def gan_load(req):
+    wanna_label = 1  # will be replaced with custom input
+    if gen_cgan_image(wanna_label):
+        return render(req, CGAN_PAGE, {})
+
+
+def about(req):
+    return HttpResponse('<h1> GAN Benchmark! </h1>')
